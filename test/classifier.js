b3c7c0c656d71bc28cef53e371ee72bc096fc07b
bredele <olivier.wietrich@gmail.com>
2015-11-20 17:08:51 -0700

    update tests with new API

--- test/classifier.js
+++ test/classifier.js
@@ -15,18 +15,18 @@ describe('classify', function() {
 	});
 
 	it('one training', function() {
-		natural.classify(['weather', 'sun', 'cold', 'cold'], 'weather');
-		assert.equal(natural.categorize(['outside', 'cold']), 'weather');
+		natural.train(['weather', 'sun', 'cold', 'cold'], 'weather');
+		assert.equal(natural.guess(['outside', 'cold']), 'weather');
 	});
 
 	it('multiple trainings', function() {
-		natural.classify(['write', 'code'], 'computing');
-		natural.classify(['script', 'code'], 'computing');
-		natural.classify(['write', 'book'], 'literature');
-		natural.classify(['read', 'book'], 'literature');
-		natural.classify(['study', 'book'], 'literature');
-		assert.equal(natural.categorize(['bug', 'code']), 'computing');
-		assert.equal(natural.categorize(['read', 'thing']), 'literature');
+		natural.train(['write', 'code'], 'computing');
+		natural.train(['script', 'code'], 'computing');
+		natural.train(['write', 'book'], 'literature');
+		natural.train(['read', 'book'], 'literature');
+		natural.train(['study', 'book'], 'literature');
+		assert.equal(natural.guess(['bug', 'code']), 'computing');
+		assert.equal(natural.guess(['read', 'thing']), 'literature');
 	});
 
 
